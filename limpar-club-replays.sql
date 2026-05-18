df93ec4073145c2c49416efee5f60e616b577a50
8linksapp-maker <8links.app@gmail.com>
2026-05-18 17:14:00 -0300

    Clean repository: remove temporary and unused files

--- limpar-club-replays.sql
+++
@@ -1,9 +0,0 @@
--- ============================================
--- Limpar TODOS os replays e sessões do Club
--- ============================================
-
--- Deletar todos os replays
-DELETE FROM club_replays;
-
--- Deletar todas as sessões
-DELETE FROM club_sessions;
